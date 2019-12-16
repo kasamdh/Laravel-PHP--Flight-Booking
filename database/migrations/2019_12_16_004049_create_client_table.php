f464ee966c4b4bb407d6f8c3098db87b2437d4bf
Kasam <kasamdh@hotmail.com>
2019-12-15 19:47:31 -0500

    Create Client table filed added

--- database/migrations/2019_12_16_004049_create_client_table.php
+++ database/migrations/2019_12_16_004049_create_client_table.php
@@ -14,7 +14,14 @@ class CreateClientTable extends Migration
     public function up()
     {
         Schema::create('client', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('client_id');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->bigInteger('phone');
+            $table->string('email');
+            $table->string('passport');
+            $table->string('iata_country_code');
             $table->timestamps();
         });
     }
