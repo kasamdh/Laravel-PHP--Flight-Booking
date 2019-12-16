4bcbb3027700ef7c817c81c227fd1eceda234172
Kasam <kasamdh@hotmail.com>
2019-12-16 18:34:17 -0500

    model added

--- app/Client.php
+++ app/Client.php
@@ -8,12 +8,14 @@ class Client extends Model
 {
 
     protected $fillable = [
-        '',
-        '',
-        '',
-        '',
-        '',
-        ''       
+        'client_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'phone',
+        'email',
+        'passport';
+        'iata_country_code'       
     ];
 
 
