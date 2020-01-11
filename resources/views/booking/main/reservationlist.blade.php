e8e0955b222640034c5299fdd0b2ad3f98f6a4a0
Kasam <kasamdh@hotmail.com>
2020-01-11 15:53:10 -0500

    Reservation list

--- resources/views/booking/main/reservationlist.blade.php
+++ resources/views/booking/main/reservationlist.blade.php
@@ -11,25 +11,8 @@
     <title>Reservation List</title>
   </head>
   <body>
-    <div class="container py-3">
-    <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
+    <div class="container">
+        
     </div>
 
     <!-- Optional JavaScript -->
