b184a4d23a966710f4c58ad2b587fedafaafbda3
Kasam <kasamdh@hotmail.com>
2020-01-21 18:18:22 -0500

    blade

--- resources/views/booking/main/reservationlist.blade.php
+++ resources/views/booking/main/reservationlist.blade.php
@@ -22,10 +22,10 @@
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
+      <!-- <th scope="row">1</th>
       <td>Mark</td>
       <td>Otto</td>
-      <td>@mdo</td>
+      <td>@mdo</td> -->
     </tr>
     <tr>
       <th scope="row">2</th>
