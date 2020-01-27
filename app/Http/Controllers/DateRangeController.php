116a59f86a7484092a75c566d8b2ecb12534d611
Kasam <kasamdh@hotmail.com>
2020-01-27 18:35:33 -0500

    data range controller

--- app/Http/Controllers/DateRangeController.php
+++ app/Http/Controllers/DateRangeController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class DateRangeController extends Controller
 {
-    //
+    function index(Request $request)
+    {
+     if(request()->ajax())
+     {
+      if(!empty($request->from_date))
+      {
+       $data = DB::table('tbl_order')
+         ->whereBetween('order_date', array($request->from_date, $request->to_date))
+         ->get();
+      }
+      else
+      {
+       $data = DB::table('tbl_order')
+         ->get();
+      }
+      return datatables()->of($data)->make(true);
+     }
+     return view('daterange');
+    }
 }
