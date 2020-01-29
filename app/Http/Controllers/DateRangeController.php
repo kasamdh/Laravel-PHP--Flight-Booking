5d178554db1daf840610f9660929bdc11e2db0da
Kasam <kasamdh@hotmail.com>
2020-01-28 21:53:36 -0500

    conttroller

--- app/Http/Controllers/DateRangeController.php
+++ app/Http/Controllers/DateRangeController.php
@@ -7,23 +7,23 @@ use DB;
 
 class DateRangeController extends Controller
 {
-    // function index(Request $request)
-    // {
-    //  if(request()->ajax())
-    //  {
-    //   if(!empty($request->from_date))
-    //   {
-    //    $data = DB::table('tbl_order')
-    //      ->whereBetween('order_date', array($request->from_date, $request->to_date))
-    //      ->get();
-    //   }
-    //   else
-    //   {
-    //    $data = DB::table('tbl_order')
-    //      ->get();
-    //   }
-    //   return datatables()->of($data)->make(true);
-    //  }
-    //  return view('daterange');
-    // }
+    function index(Request $request)
+    {
+     if(request()->ajax())
+     {
+      if(!empty($request->from_date))
+      {
+       $data = DB::table('tbl_order')
+         ->whereBetween('order_date', array($request->from_date, $request->to_date))
+         ->get();
+      }
+      else
+      {
+       $data = DB::table('tbl_order')
+         ->get();
+      }
+      return datatables()->of($data)->make(true);
+     }
+     return view('daterange');
+    }
 }
